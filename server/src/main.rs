311302b13580e043fd3ca30e1694b61ce5ab7788
Sam Behrens <sbehrens1414@gmail.com>
2026-04-14 23:29:09 -0500

    Fix package name

--- server/src/main.rs
+++ server/src/main.rs
@@ -40,7 +40,7 @@ async fn main() -> anyhow::Result<()> {
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::try_from_default_env()
-            .unwrap_or_else(|_| "aegis_sync_server=debug,tower_http=debug".into()))
+            .unwrap_or_else(|_| "server=debug,tower_http=debug".into()))
         .with(tracing_subscriber::fmt::layer())
         .init();
 
